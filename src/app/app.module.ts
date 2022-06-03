898f453f3563f0df55885fcb64920d529f758c3c
Jacob G <jrgrant560@gmail.com>
2022-06-03 16:44:03 -0700

    cleaned folders for merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './components/calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CalculatorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
