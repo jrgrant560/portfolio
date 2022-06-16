e7237c26f482132ddce60f182bad20e06beaa10d
Jacob G <jrgrant560@gmail.com>
2022-06-16 16:30:08 -0700

    modified some css

--- src/app/components/projects/tictactoe/square/square.component.ts
+++ src/app/components/projects/tictactoe/square/square.component.ts
@@ -3,11 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-tictactoe-square',
   template: `
-      <button nbButton *ngIf="!value">{{value}}</button>
-      <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-      <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+      <button *ngIf="!value">{{value}}</button>
+      <button *ngIf="value == 'X'">{{ value }}</button>
+      <button *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 3em !important; }']
+  styleUrls: ['./square.component.css']
 })
 //this is a 'Dumb' component, because it can't modify its own state; it can only be changed by the parent
 export class TictactoeSquareComponent {
