b15369444e2a65703a4b5d96c709c6b1ca264f26
Jacob G <jrgrant560@gmail.com>
2022-07-01 00:13:13 -0700

    completed Experience Page Layout

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -115,7 +115,7 @@ export class ExperienceComponent implements OnInit {
   ExpDataModelEmployers: ExpDataModel[] = [
     {
       id: 12,
-      imgSrc: "assets\\images\\Tokens\\Tools\\Token-WBP-01.png",
+      imgSrc: "assets\\images\\Tokens\\Other\\Token-WBP-01.png",
       title: "Western Business Products",
       article: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
